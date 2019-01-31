e910f18045d5e71e04f294cf69fc869cb973dfa9
quang1610 <nguyenqu2@grinnell.edu>
2019-01-30 19:50:52 -0600

    final version of Assignment 1 with comment, using Eclipse. The whole assignment is in package assignment1.

--- src/assignment1/CaesarCipher.java
+++ src/assignment1/CaesarCipher.java
@@ -5,12 +5,21 @@ import java.io.*;
 
 public class CaesarCipher {
 
+  /*
+   * input: an array of Strings
+   * 
+   * the first argument is either encode or decode
+   * the second argument is an alphabetical lower-case non-white space String.
+   * 
+   * output: print 26 possible encryptions or decryptions for the given string.
+   * 
+   * throw error if the argument is in wrong format.
+   */
   public static void main(String[] args) throws Exception {
     
-    PrintWriter pen = new PrintWriter(System.out, true);
-    
     if (args.length != 2) {
-      pen.println("Incorrect number of parameters");
+      System.err.println("Incorrect number of parameters");
+      System.exit(2);
     } else {
       
       if (args[0].equals("encode")) {
@@ -20,11 +29,17 @@ public class CaesarCipher {
         
         printDecodedString(args[1]);
       } else {
-        pen.println("Valid options are \"encode\" or \"decode\"");
+        System.err.println("Valid options are \"encode\" or \"decode\"");
+        System.exit(1);
       }
     }
   }
 
+  /*
+   * input: a String to be encoded
+   * 
+   * output: print 26 lines with all 26 possible encryptions of the input using Caesar cipher.
+   */
   public static void printEncodedString(String str) {
 
     PrintWriter pen = new PrintWriter(System.out, true);
@@ -34,15 +49,28 @@ public class CaesarCipher {
     }
   }
 
+  /*
+   * input: a String to be encoded
+   *        an integer key (from 0 to 25) to encode.
+   * output: return the encoded String using input key and Caesar cipher.
+   *
+   */
   public static String encode(String str, int key) {
     String encoded_string = new String();
     for (int n = 0; n < str.length(); n++) {
+      
+      //charInt is the integer representation of the encoded char if we consider 'a' is 0.
       int charInt = mod(str.charAt(n) - 'a' + key, 26);
       encoded_string += (char)(charInt + (int)'a');
     }
     return encoded_string;
   }
 
+  /*
+   * input: a String to be decoded
+   * 
+   * output: print 26 lines with all 26 possible decryptions of the input using Caesar cipher.
+   */
   public static void printDecodedString(String str) {
     PrintWriter pen = new PrintWriter(System.out, true);
 
@@ -51,15 +79,27 @@ public class CaesarCipher {
     }
   }
   
+  /*
+   * input: a String to be decode.
+   *        an integer key (from 0 to 25) to decode.
+   * output: return the decoded String using input key and Caesar cipher.
+   *
+   */
   public static String decode(String str, int key) {
     String decoded_string = new String();
     for (int n = 0; n < str.length(); n++) {
+      
+    //charInt is the integer representation of the decoded char if we consider 'a' is 0.
       int charInt = mod(str.charAt(n) - 'a' - key, 26);
       decoded_string += (char)(charInt + (int)'a');
     }
     return decoded_string;
   }
   
+  /*
+   * input: integer value and integer modulus
+   * output: return value mod modulus (between 0 and modulus)
+   */
   public static int mod(int value, int modulus) {
     int returnValue = value % modulus;
     
--- src/assignment1/VigenereCipher.java
+++ src/assignment1/VigenereCipher.java
@@ -1,5 +1,82 @@
 package assignment1;
 
+import java.io.PrintWriter;
+
 public class VigenereCipher {
 
+  /*
+   * input: an array of Strings
+   * 
+   * the first argument is either encode or decode. The second argument is an alphabetical
+   * lower-case non-white space String. The third argument is the encryption key, an alphabetical
+   * lower-case non-white space String.
+   * 
+   * output: print the encryptions or decryptions for the given String using the key and Vigenere
+   * Cipher.
+   * 
+   * throw error if the arguments are in wrong format.
+   */
+  public static void main(String[] args) throws Exception {
+
+    if (args.length != 3) {
+      System.err.println("Incorrect number of parameters");
+      System.exit(2);
+    } else {
+      PrintWriter pen = new PrintWriter(System.out, true);
+      if (args[0].equals("encode")) {
+        pen.println(encode(args[1], args[2]));
+
+      } else if (args[0].equals("decode")) {
+
+        pen.println(decode(args[1], args[2]));
+      } else {
+        System.err.println("Valid options are \"encode\" or \"decode\"");
+        System.exit(1);
+      }
+    }
+  }
+
+  /*
+   * input: a String to be encoded and a String key to encode. 
+   * output: return the encoded String using input key and Vignere cipher.
+   *
+   */
+  public static String encode(String str, String key) {
+    String encoded_string = new String();
+    for (int n = 0; n < str.length(); n++) {
+
+      // charInt is the integer representation of the decoded char if we consider 'a' is 0.
+      int charInt = mod(str.charAt(n) + key.charAt(n % key.length()) - 2 * 'a', 26);
+      encoded_string += (char) (charInt + (int) 'a');
+    }
+    return encoded_string;
+  }
+
+  /*
+   * input: a String to be decoded and String key to decode. 
+   * output: return the decoded String using input key and Vignere cipher.
+   */
+  public static String decode(String str, String key) {
+    String decoded_string = new String();
+    for (int n = 0; n < str.length(); n++) {
+
+      // charInt is the integer representation of the decoded char if we consider 'a' is 0.
+      int charInt = mod(str.charAt(n) - key.charAt(n % key.length()), 26);
+      decoded_string += (char) (charInt + (int) 'a');
+    }
+    return decoded_string;
+  }
+
+  /*
+   * input: integer value and integer modulus.
+   * output: return value mod modulus (between 0 and modulus)
+   */
+  public static int mod(int value, int modulus) {
+    int returnValue = value % modulus;
+
+    if (returnValue < 0) {
+      returnValue += modulus;
+    }
+    return returnValue;
+  }
 }
